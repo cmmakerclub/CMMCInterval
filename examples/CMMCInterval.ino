355274594960bfbdf87daa24dfbb0a1b4cf4f28a
Nat Weerawan <nat.wrw@gmail.com>
2016-02-16 11:57:14 +0700

    Add examples directory

--- examples/CMMCInterval.ino
+++ examples/CMMCInterval.ino
@@ -3,8 +3,8 @@
 void HelloWorld();
 void LoveYou();
 
-CMMCInterval myInterval1(1000,HelloWorld);
-CMMCInterval myInterval2(500,LoveYou);
+CMMCInterval myInterval1(1000, HelloWorld);
+CMMCInterval myInterval2(500, LoveYou);
 
 void setup() {
   Serial.begin(9600);
@@ -14,10 +14,12 @@ void loop() {
   myInterval1.Update();
   myInterval2.Update();
 }
+
 void HelloWorld()
 {
     Serial.println("Helloworld");
 }
+
 void LoveYou()
 {
     Serial.println("LoveYou");
